931d9a37069be7c6d16a3be16a440a11338614ec
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2019-11-30 12:20:11 +0100

    fix typo

--- src/main/java/net/jovacorp/bmj/beanbox/bean/ImageChooserBeanInfo.java
+++ src/main/java/net/jovacorp/bmj/beanbox/bean/ImageChooserBeanInfo.java
@@ -15,7 +15,7 @@ public class ImageChooserBeanInfo extends SimpleBeanInfo {
             ImageChooser.class,
             "image",
             Listener.class,
-            new String[] {"sourcedChange"},
+            new String[] {"sourceChange"},
             "addImageListener",
             "removeImageListener")
       };
